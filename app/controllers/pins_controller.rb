46cd9a98c85153923a91fbb26a0a8146256b35bb
pantomsky <twitkowski05@gmail.com>
2017-02-02 21:55:27 +0000

    powiązanie pinów z użytkownikami, uwierzytaelnianie

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /pins
   # GET /pins.json
@@ -14,7 +16,7 @@ class PinsController < ApplicationController
 
   # GET /pins/new
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   # GET /pins/1/edit
@@ -24,11 +26,11 @@ class PinsController < ApplicationController
   # POST /pins
   # POST /pins.json
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
     respond_to do |format|
       if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
+        format.html { redirect_to @pin, notice: 'Pin utworzony pomyślnie.' }
         format.json { render :show, status: :created, location: @pin }
       else
         format.html { render :new }
@@ -42,7 +44,7 @@ class PinsController < ApplicationController
   def update
     respond_to do |format|
       if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+        format.html { redirect_to @pin, notice: 'Pin zmieniony pomyślnie.' }
         format.json { render :show, status: :ok, location: @pin }
       else
         format.html { render :edit }
@@ -56,7 +58,7 @@ class PinsController < ApplicationController
   def destroy
     @pin.destroy
     respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
+      format.html { redirect_to pins_url, notice: 'Pin usuniety pomyślnie.' }
       format.json { head :no_content }
     end
   end
@@ -71,4 +73,11 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:decription)
     end
+    
+    # Definicja uprawnionego uzytkownika
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Nie jesteś uprawnionym użytkownikiem" if @pin.nil?
+    end
+    
 end
